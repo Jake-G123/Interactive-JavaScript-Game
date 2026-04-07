1fa7ec75e578c89ce598053d7302909f598bd02d
Jake-G123 <jakelgerken@gmail.com>
2026-04-07 11:00:59 -0700

    Finished second step

---
+++ game.js
@@ -0,0 +1,12 @@
+let score=0;
+let pointsPerClick=1;
+
+function updateDisplay() {
+    document.getElementById('score-display').textContent = 'Score: ' + score;
+    document.getElementById('rate-display').textContent = 'Points per click: ' + pointsPerlick;
+}
+
+document.getElementById('click-btn').addEventListener('click', function() {
+    score += pointsPerClick;
+    updateDisplay();
+});
