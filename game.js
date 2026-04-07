21cd39aca7ff6de04a292456ba4022def7141c9b
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-04-07 11:19:20 -0700

    solving the merge conflict

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ let pointsPerClick=1;
 
 function updateDisplay() {
     document.getElementById('score-display').textContent = 'Score: ' + score;
-    document.getElementById('rate-display').textContent = 'Points per click: ' + pointsPerlick;
+    document.getElementById('rate-display').textContent = 'Points per click: ' + pointsPerClick;
 }
 
 document.getElementById('click-btn').addEventListener('click', function() {
